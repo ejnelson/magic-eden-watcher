1f8b4fc6a6c13d4873ef2717a1601fe754e262e6
z0050qg <erik.nelson@target.com>
2021-12-23 18:05:37 -0600

    added more floor prices

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
-import './App.css';
-import {Watcher} from './components/Watcher';
-import {useState}from 'react';
+import './App.css'
+import { Watcher } from './components/Watcher'
+import { useState } from 'react'
 
 function App() {
-const [projectName, setProjectName] = useState('')
-const [watcherName, setWatcherName] = useState('')
-  const handleChange=(event)=> {
-    setProjectName(event.target.value);
-  }
+    const [projectName, setProjectName] = useState('')
+    const [watcherName, setWatcherName] = useState('')
 
-  const handleSubmit=(event)=> {
-    event.preventDefault();
-    setWatcherName(projectName)
-    // alert('A name was submitted: ' + projectName);
-  }
+    const handleChange = (event) => {
+        setProjectName(event.target.value)
+    }
 
-  return (
-    <div className="App">
-      <label>
-        Magic eden string name:
-        <input type="text" name="projectName"  onChange={handleChange}/>
-      </label>
-      <button type="button"  onClick={handleSubmit} disabled={!!watcherName}>Submit</button>
-      {watcherName&&<Watcher type={'Floor'} projectName={watcherName}/>}
-      {watcherName&&<Watcher type={'Listed'} projectName={watcherName}/>}
-      {watcherName&&<Watcher type={'Volume'} projectName={watcherName}/>}
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setWatcherName(projectName)
+        // alert('A name was submitted: ' + projectName);
+    }
 
-    </div>
-  );
+    return (
+        <div className="App">
+            <label>
+                Magic eden string name:
+                <input type="text" name="projectName" onChange={handleChange} />
+            </label>
+            <button
+                type="button"
+                onClick={handleSubmit}
+                disabled={!!watcherName}
+            >
+                Submit
+            </button>{' '}
+            tipjar: ejn.sol
+            {watcherName && (
+                <Watcher chartType={'Floor'} projectName={watcherName} />
+            )}
+            {watcherName && (
+                <Watcher chartType={'Listed'} projectName={watcherName} />
+            )}
+            {watcherName && (
+                <Watcher
+                    chartType={'Volume/Floor Price'}
+                    projectName={watcherName}
+                />
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/Watcher.js
+++ src/components/Watcher.js
@@ -1,105 +1,137 @@
-import {useRef, } from "react";
-// Import Highcharts
-import Highcharts from "highcharts/highstock";
-//import HighchartsReact from "./HighchartsReact.min.js";
-import HighchartsReact from "highcharts-react-official";
-import axios from 'axios';
+import { useRef, useEffect } from 'react'
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official'
+import axios from 'axios'
 
-// import HC_more from "highcharts/highcharts-more"; //module
-// HC_more(Highcharts); //init module
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const LISTED_NFT_API =
+    'https://api-mainnet.magiceden.io/rpc/getListedNFTsByQuery'
 
 const API = 'https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/'
 
-const LAMPORTS_PER_SOL = 1000000000;
+// const LAMPORTS_PER_SOL = 1000000000
 
-export const Watcher=({projectName,type})=> {
-const chartComponent=useRef(null); 
+export const Watcher = ({ projectName, chartType }) => {
+    const chartComponent = useRef(null)
 
-const options= {
-  time: {
-      useUTC: false
-  },
-  
-  rangeSelector: {
-      buttons: [{
-          count: 1,
-          type: 'minute',
-          text: '1M'
-      }, {
-          count: 5,
-          type: 'minute',
-          text: '5M'
-      }, {
-          type: 'all',
-          text: 'All'
-      }],
-      inputEnabled: false,
-      selected: 0
-  },
-  
-  title: {
-      text: type
-  },
-  
-  exporting: {
-      enabled: false
-  },
-  
-  series: [{
-      name: 'chart',
-      data: [ ]
-  }],
-  credits: {
-    enabled: false
-  },
-  }
+    useEffect(() => {
+        if (chartType === 'Floor') {
+            chartComponent.current.chart.addSeries({
+                name: 'Floor',
+                data: [],
+            })
+            chartComponent.current.chart.addSeries({
+                name: '5th from Floor',
+                data: [],
+            })
+            chartComponent.current.chart.addSeries({
+                name: '10th from Floor',
+                data: [],
+            })
+        } else {
+            chartComponent.current.chart.addSeries({
+                name: chartType,
+                data: [],
+            })
+        }
+    }, [])
+    const options = {
+        time: {
+            useUTC: false,
+        },
 
-const getNewData = async () => {
-  if(chartComponent.current?.chart?.series){
+        rangeSelector: {
+            buttons: [
+                {
+                    count: 1,
+                    type: 'minute',
+                    text: '1M',
+                },
+                {
+                    count: 5,
+                    type: 'minute',
+                    text: '5M',
+                },
+                {
+                    type: 'all',
+                    text: 'All',
+                },
+            ],
+            inputEnabled: false,
+            selected: 0,
+        },
 
-    let response = await axios.get(
-      `${API}${projectName}`,
-      )
-      const series = chartComponent.current.chart.series[0]
-      const results = response.data.results
-      console.log('results', results)
-const newData = type==='Floor'? results.floorPrice/LAMPORTS_PER_SOL: type ==='Listed'? results.listedCount:results.volumeAll/LAMPORTS_PER_SOL
-      series.addPoint([(new Date()).getTime(), newData], true, false);
+        title: {
+            text: chartType,
+        },
 
+        exporting: {
+            enabled: false,
+        },
+
+        credits: {
+            enabled: false,
+        },
     }
- 
+
+    const getNewData = async () => {
+        if (chartComponent.current?.chart?.series) {
+            if (chartType === 'Floor') {
+                const seriesFloor = chartComponent.current.chart.series[0]
+                const series5th = chartComponent.current.chart.series[1]
+                const series10th = chartComponent.current.chart.series[2]
+
+                let response = await axios.get(LISTED_NFT_API, {
+                    params: {
+                        q: {
+                            $match: { collectionSymbol: projectName },
+                            $sort: { takerAmount: 1, createdAt: -1 },
+                            $skip: 0,
+                            $limit: 10,
+                        },
+                    },
+                })
+                const floorPrice = response.data.results[0].price
+                const fifthFromFloorPrice = response.data.results[4].price
+                const tenthFromFloorPrice = response.data.results[9].price
+
+                seriesFloor.addPoint(
+                    [new Date().getTime(), floorPrice],
+                    false,
+                    false
+                )
+                series5th.addPoint(
+                    [new Date().getTime(), fifthFromFloorPrice],
+                    false,
+                    false
+                )
+
+                series10th.addPoint(
+                    [new Date().getTime(), tenthFromFloorPrice],
+                    true,
+                    false
+                )
+            } else {
+                const series = chartComponent.current.chart.series[0]
+                let response = await axios.get(`${API}${projectName}`)
+
+                const results = response.data.results
+                const newData =
+                    chartType === 'Listed'
+                        ? results.listedCount
+                        : results.volumeAll / results.floorPrice
+                series.addPoint([new Date().getTime(), newData], true, false)
+            }
+        }
+    }
+    setInterval(getNewData, 1000)
+    return (
+        <>
+            <HighchartsReact
+                constructorType={'stockChart'}
+                ref={chartComponent}
+                highcharts={Highcharts}
+                options={options}
+            />
+        </>
+    )
 }
-setInterval(getNewData, 1000);
-  return (
-    <>
-      <HighchartsReact
-        constructorType={"stockChart"}
-        ref={chartComponent}
-        highcharts={Highcharts}
-        options={options}
-      />
-      
-
-    </>
-    );
-}
-
-
